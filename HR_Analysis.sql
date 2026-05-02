cc55dd2ad969454ce1ffb280a8fef2f0d0d9b195
Anika Jairath <ajairath2000@gmail.com>
2026-05-02 12:01:52 +0530

    Updated HR_Analysis.sql

--- HR_Analysis.sql
+++ HR_Analysis.sql
@@ -21,20 +21,22 @@ SELECT Department, COUNT(Employee_ID) AS Emp_Distribution_Per_Dept FROM employee
 GROUP BY Department ORDER BY Emp_Distribution_Per_Dept;
 
 -- Attrition per Salary Groups
-SELECT Salary_Group, Attrition, COUNT(Employee_ID) AS Attrition_by_Salary_Group FROM employees 
-GROUP BY Salary_Group, Attrition 
-HAVING Attrition = 'Yes' 
+SELECT Salary_Group, COUNT(Employee_ID) AS Attrition_by_Salary_Group FROM employees 
+WHERE Attrition = 'Yes'
+GROUP BY Salary_Group 
 ORDER BY Attrition_by_Salary_Group DESC;
 
 -- Attrition by Years of experience
 SELECT Experience_Range, COUNT(Employee_ID) AS Attrition_per_Experince_Range FROM employees 
-GROUP BY Experience_Range, Attrition 
-HAVING Attrition = 'Yes' ORDER BY Attrition_per_Experince_Range DESC;
+WHERE Attrition = 'Yes'
+GROUP BY Experience_Range 
+ORDER BY Attrition_per_Experince_Range DESC;
 
 -- Experience Range with the highest attrition
-SELECT Experience_Range, Attrition, COUNT(Employee_ID) AS Attrition_per_Experience_Range 
+SELECT Experience_Range, COUNT(Employee_ID) AS Attrition_per_Experience_Range 
 FROM employees 
-GROUP BY Experience_Range, Attrition HAVING Attrition = 'Yes' 
+WHERE Attrition = 'Yes'
+GROUP BY Experience_Range 
 ORDER BY Attrition_per_Experience_Range DESC LIMIT 1;
 
 -- Attrition Distribution by Job Satisfaction
@@ -44,9 +46,10 @@ GROUP BY Job_Satisfaction, Attrition
 ORDER BY Job_Satisfaction;
 
 -- Top 3 satisfaction levels with the highest attrition
-SELECT Job_Satisfaction, Attrition, COUNT(Employee_ID) AS Attrition_by_Job_Satisfaction 
-FROM employees GROUP BY Job_Satisfaction, Attrition 
-HAVING Attrition = 'Yes' 
+SELECT Job_Satisfaction, COUNT(Employee_ID) AS Attrition_by_Job_Satisfaction 
+FROM employees 
+WHERE Attrition = 'Yes'
+GROUP BY Job_Satisfaction 
 ORDER BY Attrition_by_Job_Satisfaction DESC LIMIT 3;
 
 -- Average Satisfaction of employees who left vs who stayed
@@ -54,21 +57,12 @@ SELECT Attrition, AVG(Job_Satisfaction) AS Average_Satisfaction FROM employees G
 
 --  Comparison of attrition across different work-life balance levels 
 SELECT Work_Life_Balance, COUNT(Employee_ID) AS Attrition_by_Work_Life_Balance FROM employees 
-GROUP BY Work_Life_Balance, Attrition 
-HAVING Attrition = 'Yes' 
+WHERE Attrition = 'Yes'
+GROUP BY Work_Life_Balance 
 ORDER BY Attrition_by_Work_Life_Balance DESC;
 
 -- Distribution of attrition by salary and work-life balance
 SELECT  Salary_Group, Work_Life_Balance, Attrition, COUNT(Employee_ID) AS Count
 FROM employees
 GROUP BY Salary_Group, Work_Life_Balance, Attrition
-ORDER BY Salary_Group, Work_Life_Balance; 
-
-
-
-
-
-
-
-
-
+ORDER BY Salary_Group, Work_Life_Balance;
